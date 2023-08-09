95067501c91673c89fc9ffa639588f9229f23472
Marek Suchánek <msuchane@redhat.com>
2023-08-09 17:17:07 +0200

    Adapt logging and error reporting from newdoc

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
-mod cli;
+// Enable additional clippy lints by default.
+#![warn(
+    clippy::pedantic,
+    clippy::unwrap_used,
+    clippy::clone_on_ref_ptr,
+    clippy::todo
+)]
+// Disable the documentation clippy lint, so that it stops suggesting backticks around AsciiDoc.
+#![allow(clippy::doc_markdown)]
+// Forbid unsafe code in this program.
+#![forbid(unsafe_code)]
 
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+use color_eyre::Result;
+
+mod cli;
+mod logging;
+
+fn main() -> Result<()> {
+    // Enable full-featured error logging.
+    color_eyre::install()?;
+
     let args = cli::get_args();
+
+    // Initialize the logging system based on the set verbosity
+    logging::initialize_logger(args.verbose)?;
+
     println!("{:#?}", args);
+
+    let file = fs::read_to_string(args.file)?;
+    println!("{file}");
+
+    Ok(())
 }
